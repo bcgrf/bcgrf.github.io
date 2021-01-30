74a615b5f6f00851f498e40bc27ea718c5be08df
bcgrf <bcgrfindodax@gmail.com>
2021-01-30 11:36:18 +0700

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'bdpv': "url('/public/img/default.png')"
+      })
+    },
   },
   variants: {
     extend: {},
